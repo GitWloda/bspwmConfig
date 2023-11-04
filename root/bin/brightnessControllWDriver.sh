c354e1338f30ef02c3a3818432f5ed3d8093c219
GitWloda <raffaelwloda@gmail.com>
2023-11-04 23:43:57 +0100

    adding some config file

---
+++ root/bin/brightnessControllWDriver.sh
@@ -0,0 +1,13 @@
+light=$(cat /sys/class/backlight/nvidia_wmi_ec_backlight/brightness)
+if [ $1 == D ]; then
+	if [ "$light" -gt "9" ]; then
+		sh -c 'echo $(($(cat /sys/class/backlight/nvidia_wmi_ec_backlight/brightness)-5)) > /sys/class/backlight/nvidia_wmi_ec_backlight/brightness'
+	fi
+elif [ $1 = U ]; then
+	if [ "$light" -lt "99" ]; then
+		sh -c 'echo $(($(cat /sys/class/backlight/nvidia_wmi_ec_backlight/brightness)+5)) > /sys/class/backlight/nvidia_wmi_ec_backlight/brightness'
+	fi
+elif [ $1 = G ]; then
+	echo $light
+fi
+
