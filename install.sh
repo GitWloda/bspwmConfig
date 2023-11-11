5ff5b6677d17cf7efb52a1acdf456746e9c07f92
GitWloda <raffaelwloda@gmail.com>
2023-11-11 15:55:19 +0000

    config update: add picom and some wallpaper, edit old things

--- install.sh
+++ install.sh
@@ -1,23 +1,27 @@
+#upgrade pacman
+echo "upgrade pacman and shaur"
+doas pacman -Suyy
+shaur -U
+
 #isntall base things
 echo "installing base things"
-doas pacman --needed -S base base-devel linux-firmware intel-ucode doas sudo networkmanager
-#linux-zen linux-zen-headers
+doas pacman --needed -S base base-devel linux-firmware intel-ucode doas sudo networkmanager linux-zen linux-zen-headers
 
 #install driver
 echo "installing driver"
-doas pacman --needed -S nvidia-utils nvidia-prime mesa intel-gpu-tools xorg
+doas pacman --needed -S nvidia-open-dkms nvidia-utils nvidia-prime mesa intel-gpu-tools xorg pulseaudio pavucontrol
 shaur -S asusctl envycontrol
-#nvidia-open-dkms
 
 #install screen rice
 echo "installing screen rice"
-doas pacman --needed -S sxhkd bspwm polybar rofi dunst bat feh python-pywal alacritty exa
+doas pacman --needed -S sxhkd bspwm polybar rofi dunst bat feh python-pywal alacritty exa noto-fonts-emoji
+shaur -S picom-allusive
 
 #install tecnical rice
 echo "installing technical rice"
-doas pacman --needed -S zip unzip git curl gzip jq skim fd git zsh numlockx
+doas pacman --needed -S zip unzip git curl gzip jq skim fd git zsh numlockx 
+shaur -S ueberzugpp yazi-git
 
 #install rest
 echo "install rest of rice"
-shaur -S battop gotop rog-control-center ttf-ubraille
-
+shaur -S battop gotop rog-control-center ttf-ubraille nchat 
